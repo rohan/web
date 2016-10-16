fe0dd35029da758bde6a7ae36025ce79cec666bf
Rohan Ramchand <rohan@ramchand.me>
2016-10-16 18:46:09 -0400

    now with bootstrap for responsiveness

--- html/js/script.js
+++ html/js/script.js
@@ -1,22 +1,37 @@
+var container = $('body'),
+scrollTo = $('#details');
+
 $(document).ready(function() {
   $('#about-link').click(function() {
   	$("#details").children().hide();
   	$('#about').show();
-    $("html, body, #details").scrollTop(0);
+    // $("html, body, #details").scrollTop(0);
     // $("#info").height($("#about").height())
+    container.animate({
+        scrollTop: $('#details').offset().top
+    }, 1000);
   });
 
   $('#courses-link').click(function() {
     $("#details").children().hide();
     $('#courses').show();
     // $("#info").height($("#courses").height())
-    $("html, body, #details").scrollTop(0);
+    // $("html, body, #details").scrollTop(0);
+    container.animate({
+        scrollTop: $('#details').offset().top
+    }, 1000);
   });
 
   $('#projects-link').click(function() {
     $("#details").children().hide();
     $('#projects').show();
-    $("html, body, #details").scrollTop(0);
+    // $("html, body, #details").scrollTop(0);
     // $("#info").height($("#projects").height())
+
+    container.animate({
+        scrollTop: $('#details').offset().top
+    }, 1000);
   });
+
+
 });
